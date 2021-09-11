d6e7e7d3c5e90c0457d9b57d657c38b12b704967
diegomirhan <mirhan.diego@gmail.com>
2021-09-10 23:57:03 -0300

    weather

---
+++ weather.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/58.0.3029.110 Safari/537.3'}
+
+
+def weather(city):
+    city = city.replace(" ", "+")
+    res = requests.get(
+        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome'
+        f'&ie=UTF-8',
+        headers=headers)
+    print("Searching...\n")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    location = soup.select('#wob_loc')[0].getText().strip()
+    time = soup.select('#wob_dts')[0].getText().strip()
+    precipitation = soup.select('#wob_pp')[0].getText().strip()
+    humidity = soup.select('#wob_hm')[0].getText().strip()
+    wind = soup.select('#wob_tws')[0].getText().strip()
+    info = soup.select('#wob_dc')[0].getText().strip()
+    weathers = soup.select('#wob_tm')[0].getText().strip()
+    print(location)
+    print(time)
+    print(precipitation)
+    print(humidity)
+    print(wind)
+    print(info)
+    print(weathers + "°C")
+
+
+cities = input("Enter the Name of City ->  ")
+cities = cities + " weather"
+weather(cities)
+print("Have a Nice Day:)")
+
+
+
+
+
+
