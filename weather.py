380f2863ab3f03f39bdf5538943413e6824edb10
diegomirhan <mirhan.diego@gmail.com>
2021-09-23 07:39:06 -0300

    Update - 23-09

--- weather.py
+++ weather.py
@@ -1,4 +1,3 @@
-"""
 from bs4 import BeautifulSoup
 import requests
 
@@ -8,6 +7,7 @@ headers = {
 
 
 def cidade(city):
+    city += ' Weather'
     city = city.replace(" ", "+")
     res = requests.get(
         f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7'
@@ -26,40 +26,6 @@ def cidade(city):
            f'Clima: {climate + "ºC"} - {info} \n' \
            f'Data: {time} \n' \
            f'Precipatação: {precipitation} \n' \
-           f'Humidade: {humidity} \n' \
-"""
-# importing requests and json
-import requests
-import json
+           f'Humidade: {humidity} \n' 
 
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-API_KEY = "6ea3659178e682c04bfd3d1db3c6a362"
-
-
-def cidades(cidade):
-    # upadting the URL
-    url = BASE_URL + "q=" + cidade + "&appid=" + API_KEY
-    # HTTP request
-    response = requests.get(url)
-    # checking the status code of the request
-    if response.status_code == 200:
-        # getting data in the json format
-        data = response.json()
-        # getting the main dict block
-        main = data['main']
-        # getting temperature
-        temp = main['temp']
-        temperature = int(temp)
-        # getting the humidity
-        humidity = main['humidity']
-        # getting the pressure
-        pressure = main['pressure']
-        # weather report
-        report = data['weather']
-        return f"{temperature - 279}"
-    else:
-        # showing the error message
-        return "Error in the HTTP request"
-
-
-print(cidades('Ubatuba'))
+print(cidade('Ubatuba'))
